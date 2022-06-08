b2779f303270091796a735d2e2437bef14146f2c
frontcodelover <contact@avantjetaisriche.com>
2022-06-08 17:24:51 +0200

    Data fetch

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -4,10 +4,36 @@ import React from "react";
 import App from "next/app";
 import Head from "next/head";
 import Layout from "../components/Layout";
+import axios from 'axios'
+// import getDatasFromSpotsAPI from '../components/datasParse/getDatasFromSpotsAPI';
+
+async function getDatasFromSpotsAPI() {
+  try {
+    const data = await axios.get("http://localhost:1337/api/spots?populate=*");
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 export default class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+
+    let pageProps = {};
+    const response = await getDatasFromSpotsAPI();
+    
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    
+    // console.log(data)
+    return { pageProps, data: response.data };
+  }
+
   render() {
-    const { Component, pageProps } = this.props;
+    const { Component, pageProps, data } = this.props;
+    console.log(data);
+
     return (
       <>
         <Head>
@@ -15,7 +41,7 @@ export default class MyApp extends App {
         </Head>
 
         <Layout>
-          <Component {...pageProps} />
+          <Component {...pageProps} data={data} />
         </Layout>
       </>
     );
---
+++ frontend/pages/coucou.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+
+export default function coucou({data}) {
+  console.log(data)
+  return (
+    <div></div>
+  )
+}
